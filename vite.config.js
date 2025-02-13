e5cb423f411bb57832c155fe3b345be4d7321185
Dennis-Rudiger <146859950+Dennis-Rudiger@users.noreply.github.com>
2025-02-13 23:10:32 +0300

    Enhance Vite configuration with improved build settings and add Vercel deployment configuration

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,24 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
     outDir: 'dist',
+    sourcemap: false,
+    minify: 'terser',
+    chunkSizeWarningLimit: 1600,
     rollupOptions: {
-      onwarn(warning, warn) {
-        if (warning.code === 'MISSING_EXPORT') return
-        warn(warning)
-      }
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor';
+          }
+        },
+      },
     }
+  },
+  server: {
+    host: true
   }
 })
