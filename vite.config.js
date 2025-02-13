8bdab25a986a05c96a4d4b19ceb47a11f31e74b0
Dennis-Rudiger <146859950+Dennis-Rudiger@users.noreply.github.com>
2025-02-13 22:06:40 +0300

    modified

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      onwarn(warning, warn) {
+        if (warning.code === 'MISSING_EXPORT') return
+        warn(warning)
+      }
+    }
+  }
 })
